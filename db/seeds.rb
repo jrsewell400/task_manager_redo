385364c443ee91ddfbf2c71c601de19ad9d29745
Jordan Sewell <jrsewell400@gmail.com>
2020-03-07 21:13:13 -0700

    Update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+task_1 = Task.create(title: "Task 1", description: "This is Task 1")
+task_2 = Task.create(title: "Task 2", description: "This is Task 2")
+task_3 = Task.create(title: "Task 3", description: "This is Task 3")
+task_4 = Task.create(title: "Task 4", description: "This is Task 4")
+task_5 = Task.create(title: "Task 5", description: "This is Task 5")
