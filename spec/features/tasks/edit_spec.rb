a79d832d8788ed6137a6e83e292a07c36ceacc50
Jordan Sewell <jrsewell400@gmail.com>
2020-03-08 19:43:46 -0600

    Create task edit process to ensure a task can be edited

---
+++ spec/features/tasks/edit_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe "As a visitor,", type: :feature do 
+  describe "when I want to edit a task" do 
+    it "then I can visit an individual tasks Show Page and edit the task." do
+      task_1 = Task.create(title: "Task 1", description: "This is Task 1")
+
+      visit "/tasks/#{task_1.id}"
+
+      click_on "Edit Task"
+
+      fill_in :title, with: "Task 3"
+      fill_in :description, with: "This is Task 3"
+      click_on "Submit"
+
+      save_and_open_page
+      expect(page).to_not have_content("Task 1")
+      expect(page).to_not have_content("This is Task 1")
+      expect(page).to have_content("Task 3")
+      expect(page).to have_content("This is Task 3")
+    end 
+  end
+end
